2e53823821500acc33d38b98ce6359ac71fecc5d
André pierre <temps.jo@gmail.com>
2014-10-22 22:38:34 +0200

    Create jo.h

---
+++ jo.h
@@ -0,0 +1,16 @@
+#ifndef _jo_h_
+#define _jo_h_
+
+struct jo
+{
+  int duree;
+  int laforceh;
+  int laforceb;
+  int amplitude[4];
+  int dureefront[4];
+  int changeamplitude[4];
+  int changedureefront[4];
+  int combiendezonememoire;
+};
+
+#endif // _jo_h_
