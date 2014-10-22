ca8f08f5747c63062367a732c2bdbd06c887aeda
André pierre <temps.jo@gmail.com>
2014-10-22 22:33:44 +0200

    Create clavier.h

---
+++ clavier.h
@@ -0,0 +1,82 @@
+/* Imaginer et creer par Abadie joris*/
+/* en utilisant un concept de Andre pierre jocelyn*/
+/*Cette oeuvre est protégée et n'est divulguée et réservé que pour utilisation sous linux dans les conditions Creative common ci-dessous*/
+/* Cette oeuvre est mise à disposition selon les termes de la Licence Creative Commons Paternité - Pas d'Utilisation Commerciale - Partage des Conditions Initiales à l'Identique 2.0 France. */
+/*Aucune garantie n'est donner sur les effets de cet executable*/
+#ifndef DEF_clavier
+#define DEF_clavier
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QWidget>
+#include <QPushButton>
+#include <QSignalMapper>
+#include <QObject>
+#include <QMessageBox>
+#include <QKeyEvent>
+#include <ctype.h>
+#include <QProcess>
+#ifdef WIN32 // WIN32 is defined by all Windows 32 compilers, but not by others.
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include "convertisseur.h"
+
+using namespace std;
+class clavier : public QWidget
+{
+  Q_OBJECT
+   QObject *parent;
+
+  public:
+  clavier();
+
+  private:
+  QPushButton *piano1a_bouton;
+  QPushButton *piano2a_bouton;
+  QPushButton *piano3a_bouton;
+  QPushButton *piano4a_bouton;
+  QPushButton *piano5a_bouton;
+  QPushButton *piano6a_bouton;
+  QPushButton *piano7a_bouton;
+  QPushButton *piano8a_bouton;
+  QPushButton *piano9a_bouton;
+  QPushButton *piano10a_bouton;
+  QPushButton *piano2_bouton;
+  QPushButton *piano3_bouton;
+  QPushButton *piano4_bouton;
+  QPushButton *piano5_bouton;
+  QPushButton *piano6_bouton;
+  QPushButton *piano7_bouton;
+  QPushButton *piano8_bouton;
+  QPushButton *piano9_bouton;
+  QPushButton *piano10_bouton;
+  QSignalMapper *sm;
+  QPushButton *aa_bouton;
+  QPushButton *an_bouton;
+  QPushButton *ao_bouton;
+  QPushButton *ap_bouton;
+  QPushButton *aq_bouton;
+  QPushButton *ar_bouton;
+  QPushButton *as_bouton;
+  QPushButton *at_bouton;
+  QPushButton *au_bouton;
+  QPushButton *av_bouton;
+  QPushButton *aw_bouton;
+  QPushButton *ax_bouton;
+  QPushButton *ay_bouton;
+  QPushButton *az_bouton;
+  
+  QPushButton *bouton[26];
+
+  public slots:
+   void fonctiontouche (int c);
+   void keyPressEvent(QKeyEvent* event);
+   void aasynthe (char c, int n);
+   void fonctionpiano1 ();
+   void fonctionpiano2 ();
+
+};
+
+#endif
