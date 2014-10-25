ee002b3066f41a8cb23f64c6fabac7cb9777cc72
André pierre <temps.jo@gmail.com>
2014-10-25 05:56:09 +0200

    Create AdnForme12.h

---
+++ AdnForme12.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme12_h_
+#define _AdnForme12_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo12(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme12_h_
