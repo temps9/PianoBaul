832483101ca43a66ae3f9c1183969cdcd05261f6
André pierre <temps.jo@gmail.com>
2014-10-25 05:55:28 +0200

    Create AdnForme11.h

---
+++ AdnForme11.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme11_h_
+#define _AdnForme11_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo11(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme11_h_
