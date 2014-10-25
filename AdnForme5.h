4f2ddecee33b48263508231aaa4c1c59b967b090
André pierre <temps.jo@gmail.com>
2014-10-25 05:50:02 +0200

    Create AdnForme5.h

---
+++ AdnForme5.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme5_h_
+#define _AdnForme5_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo5(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme5_h_
