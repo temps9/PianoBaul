30b235c3c5530b8f6258da08397deac8319d8189
André pierre <temps.jo@gmail.com>
2014-10-25 06:01:51 +0200

    Create AdnForme20.h

---
+++ AdnForme20.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme20_h_
+#define _AdnForme20_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo20(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme20_h_
