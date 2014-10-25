16ea6bd6d650e811a77a990e0bc7e1b97511c6cb
André pierre <temps.jo@gmail.com>
2014-10-25 05:52:38 +0200

    Create AdnForme8.h

---
+++ AdnForme8.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme8_h_
+#define _AdnForme8_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo8(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme8_h_
