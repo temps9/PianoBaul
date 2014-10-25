c1edd12cfe5681ea32310b4dab647e67773c031c
André pierre <temps.jo@gmail.com>
2014-10-25 05:59:01 +0200

    Create AdnForme16.h

---
+++ AdnForme16.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme16_h_
+#define _AdnForme16_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo16(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme16_h_
