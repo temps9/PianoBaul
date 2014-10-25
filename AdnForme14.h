61b5d4d28221656dff59c56989b3b9c07905b6bb
André pierre <temps.jo@gmail.com>
2014-10-25 05:57:33 +0200

    Create AdnForme14.h

---
+++ AdnForme14.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme14_h_
+#define _AdnForme14_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo14(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme14_h_
