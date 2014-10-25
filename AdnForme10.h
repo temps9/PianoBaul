006ab3f470a3da8ee30abef887523da64daf5c1f
André pierre <temps.jo@gmail.com>
2014-10-25 05:54:33 +0200

    Create AdnForme10.h

---
+++ AdnForme10.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme10_h_
+#define _AdnForme10_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo10(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme10_h_
