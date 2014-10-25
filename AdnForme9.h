cd823bc7b89b69819e5cb9a0327a0214bf9d5572
André pierre <temps.jo@gmail.com>
2014-10-25 05:53:49 +0200

    Create AdnForme9.h

---
+++ AdnForme9.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme9_h_
+#define _AdnForme9_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo9(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme9_h_
