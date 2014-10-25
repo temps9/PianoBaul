6496f226ab9c22ac52040017039e57d570f9a8b5
André pierre <temps.jo@gmail.com>
2014-10-25 05:59:49 +0200

    Create AdnForme17.h

---
+++ AdnForme17.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme17_h_
+#define _AdnForme17_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo17(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme17_h_
