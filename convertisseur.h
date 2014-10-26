bf00d851084dfcf5ca68a95ef941c7845ab0ede2
André pierre <temps.jo@gmail.com>
2014-10-26 02:04:06 +0100

    Update convertisseur.h
    ajout de formes

--- convertisseur.h
+++ convertisseur.h
@@ -8,6 +8,26 @@
 
 void genereFichierJo(char c);
 void genereFichierJo1(char c);
+void genereFichierJo2(char c);
+void genereFichierJo3(char c);
+void genereFichierJo4(char c);
+void genereFichierJo5(char c);
+void genereFichierJo6(char c);
+void genereFichierJo7(char c);
+void genereFichierJo8(char c);
+void genereFichierJo9(char c);
+void genereFichierJo10(char c);
+void genereFichierJo11(char c);
+void genereFichierJo12(char c);
+void genereFichierJo13(char c);
+void genereFichierJo14(char c);
+void genereFichierJo15(char c);
+void genereFichierJo16(char c);
+void genereFichierJo17(char c);
+void genereFichierJo18(char c);
+void genereFichierJo19(char c);
+void genereFichierJo20(char c);
+
 void conversionJoWav(char c, int n);
 
 #endif // _convertisseur_h_
