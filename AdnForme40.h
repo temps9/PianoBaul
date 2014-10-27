0b9a150e3342850d38dd4f53dfe6ba9f67210e6f
André pierre <temps.jo@gmail.com>
2014-10-27 04:42:43 +0100

    Create AdnForme40.h

---
+++ AdnForme40.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme40_h_
+#define _AdnForme40_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo40(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme40_h_
