d9b60dfd88ff95566f51913139b2883ca0c492bb
André pierre <temps.jo@gmail.com>
2014-10-27 04:31:13 +0100

    Create AdnForme29.h

---
+++ AdnForme29.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme29_h_
+#define _AdnForme29_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo29(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme29_h_
