2c6d1bae20d729a22d7817982f9426810de2bbf5
André pierre <temps.jo@gmail.com>
2014-10-27 04:34:41 +0100

    Create AdnForme31.h

---
+++ AdnForme31.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme31_h_
+#define _AdnForme31_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo31(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme31_h_
