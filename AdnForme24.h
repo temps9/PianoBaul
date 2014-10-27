77394f98aa7931b079e262049ee8b179865f9076
André pierre <temps.jo@gmail.com>
2014-10-27 04:25:20 +0100

    Create AdnForme24.h

---
+++ AdnForme24.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme24_h_
+#define _AdnForme24_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo24(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme24_h_
