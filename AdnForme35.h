56f799c77eb92ffc574062439dd63230568b6043
André pierre <temps.jo@gmail.com>
2014-10-27 04:37:58 +0100

    Create AdnForme35.h

---
+++ AdnForme35.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme35_h_
+#define _AdnForme35_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo35(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme35_h_
