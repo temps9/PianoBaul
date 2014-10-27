193aa534932c74bf6f9755824a0e5aea2ab70b8a
André pierre <temps.jo@gmail.com>
2014-10-27 04:28:34 +0100

    Create AdnForme27.h

---
+++ AdnForme27.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme27_h_
+#define _AdnForme27_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo27(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme27_h_
