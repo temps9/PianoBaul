80ec42c2ee9bc91e7b6d7007efff0c467dc10f5a
André pierre <temps.jo@gmail.com>
2014-10-27 04:26:24 +0100

    Create AdnForme25.h

---
+++ AdnForme25.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme25_h_
+#define _AdnForme25_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo25(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme25_h_
