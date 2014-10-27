1d02e7931ecabc51037b8b77882bd9e394205d1c
André pierre <temps.jo@gmail.com>
2014-10-27 04:59:32 +0100

    Update convertisseur.h

--- convertisseur.h
+++ convertisseur.h
@@ -27,6 +27,26 @@ void genereFichierJo17(char c);
 void genereFichierJo18(char c);
 void genereFichierJo19(char c);
 void genereFichierJo20(char c);
+void genereFichierJo21(char c);
+void genereFichierJo22(char c);
+void genereFichierJo23(char c);
+void genereFichierJo24(char c);
+void genereFichierJo25(char c);
+void genereFichierJo26(char c);
+void genereFichierJo27(char c);
+void genereFichierJo28(char c);
+void genereFichierJo29(char c);
+void genereFichierJo30(char c);
+void genereFichierJo31(char c);
+void genereFichierJo32(char c);
+void genereFichierJo33(char c);
+void genereFichierJo34(char c);
+void genereFichierJo35(char c);
+void genereFichierJo36(char c);
+void genereFichierJo37(char c);
+void genereFichierJo38(char c);
+void genereFichierJo39(char c);
+void genereFichierJo40(char c);
 
 void conversionJoWav(char c, int n);
 
