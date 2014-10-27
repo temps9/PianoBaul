6f5ffc9bdc0a103a2e8bde1337af1327344c36cf
André pierre <temps.jo@gmail.com>
2014-10-27 04:36:57 +0100

    Create AdnForme34.h

---
+++ AdnForme34.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme34_h_
+#define _AdnForme34_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo34(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme34_h_
