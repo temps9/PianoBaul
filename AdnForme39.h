0bfe72a3267782313db8ddd90a691f2792fae869
André pierre <temps.jo@gmail.com>
2014-10-27 04:41:54 +0100

    Create AdnForme39.h

---
+++ AdnForme39.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme39_h_
+#define _AdnForme39_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo39(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme39_h_
