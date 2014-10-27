ce14c493437fe84e31b497f5cab421fed2de8f20
André pierre <temps.jo@gmail.com>
2014-10-27 04:40:32 +0100

    Create AdnForme37.h

---
+++ AdnForme37.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme37_h_
+#define _AdnForme37_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo37(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme37_h_
