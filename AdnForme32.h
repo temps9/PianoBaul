b2633c564aa735318cd99d04cc7a0e3db13b52be
André pierre <temps.jo@gmail.com>
2014-10-27 04:35:20 +0100

    Create AdnForme32.h

---
+++ AdnForme32.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme32_h_
+#define _AdnForme32_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo32(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme32_h_
