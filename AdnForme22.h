9ef1dfeb674f8296c397f26ed8f546634497fe60
André pierre <temps.jo@gmail.com>
2014-10-27 04:22:58 +0100

    Create AdnForme22.h

---
+++ AdnForme22.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme22_h_
+#define _AdnForme22_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo22(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme22_h_
