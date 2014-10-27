f86943273bff2a860d38ff4faf2f07c508bbb147
André pierre <temps.jo@gmail.com>
2014-10-27 04:29:50 +0100

    Create AdnForme28.h

---
+++ AdnForme28.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme28_h_
+#define _AdnForme28_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo28(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme28_h_
