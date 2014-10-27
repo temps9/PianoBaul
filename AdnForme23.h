2202375e508a4a6c4f94f679fa07b15b05c3208a
André pierre <temps.jo@gmail.com>
2014-10-27 04:24:09 +0100

    Create AdnForme23.h

---
+++ AdnForme23.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme23_h_
+#define _AdnForme23_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo23(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme23_h_
