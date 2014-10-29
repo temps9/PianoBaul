ec608fd8848cd6086a82b4229a415bd03fe7d3c4
André pierre <temps.jo@gmail.com>
2014-10-29 23:10:22 +0100

    Create AdnForme.cpp

---
+++ AdnForme.cpp
@@ -0,0 +1,126 @@
+#include "AdnForme.h"
+
+// Génère les fichiers c.jo et c.joa
+void genereFichierJo(char c)
+{
+
+    int mesamplifront[2]; //son du n, la référence
+    int mesdurerfront[2]; //son du n, la référence
+    int mesvarampli[2]; //son du n, la référence
+    int mesvardurer[2]; //son du n, la référence
+    int madurer; //son du n, la référence
+    int maforceh;
+    int maforceb;
+    int montremolo;
+    int maforceplus;
+    int macombiendezonememoire;
+
+
+   FILE* fichierdesfronts = NULL;
+
+ 
+    fichierdesfronts = fopen("fronts.txt", "r");
+ 
+    if (fichierdesfronts != NULL)
+    {
+        fscanf(fichierdesfronts, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d", &mesamplifront[0], &mesamplifront[1], &mesdurerfront[0], &mesdurerfront[1], &mesvarampli[0], &mesvarampli[1], &mesvardurer[0], &mesvardurer[1], &madurer, &maforceh, &maforceb, &montremolo, &maforceplus, &macombiendezonememoire);       
+ 
+        fclose(fichierdesfronts);
+
+    }
+
+/**********************************************/
+ jo joc = mesjo[c-'a'];
+
+  // Nom du fichier jo
+  char nomfichierjo[32];
+  snprintf(nomfichierjo, 32, "%c.jo", c);
+  // Nom du fichier joa
+  char nomfichierjoa[32];
+  snprintf(nomfichierjoa, 32, "%c.joa", c);
+
+  // Fichier jo
+  FILE* fichierjo;
+  if (!(fichierjo = fopen(nomfichierjo, "wb")))
+   {
+    exit(-1);
+   }
+  // Fichier joa
+  FILE* fichierjoa;
+  if (!(fichierjoa = fopen(nomfichierjoa, "wb")))
+   {
+    exit(-1);
+   }
+
+// ici commence la zone de travail pour l'apprenant
+
+mesdurerfront[0] = ((joc.lagame * mesdurerfront[0]) * 0.01);
+mesdurerfront[1] = ((joc.lagame * mesdurerfront[1]) * 0.01);
+
+
+// multiplie
+  for(int duree = madurer; duree>0; duree--)
+   {
+
+//monte
+
+    for(int laforcebbase = maforceh; laforcebbase > 0; laforcebbase--)
+     {
+      for(int i=0; i<2; i++)
+       {
+        if(i%2)
+         mesamplifront[i] -= mesvarampli[i];
+        else
+         mesamplifront[i] += mesvarampli[i];
+	
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[i], (char)mesdurerfront[i]);
+        fprintf(fichierjoa, "%d %d\n", (unsigned char)mesamplifront[i], (unsigned char)mesdurerfront[i]);
+
+       }
+     }
+//plateau
+
+
+    for(int laforcebbase = maforceb; laforcebbase > 0; laforcebbase--)
+     {
+      for(int i=0; i<2; i++)
+       {
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[i], (char)mesdurerfront[i]);
+        fprintf(fichierjoa, "%d %d\n", (unsigned char)mesamplifront[i], (unsigned char)mesdurerfront[i]);
+
+       }
+     }
+
+
+
+//descend
+
+    for(int laforcebbase = maforceh; laforcebbase > 0; laforcebbase--)
+     {
+      for(int i=0; i<2; i++)
+       {
+        if(i%2)
+         mesamplifront[i] += mesvarampli[i];
+        else
+         mesamplifront[i] -= mesvarampli[i];
+	
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[i], (char)mesdurerfront[i]);
+        fprintf(fichierjoa, "%d %d\n", (unsigned char)mesamplifront[i], (unsigned char)mesdurerfront[i]);
+
+       }
+     }
+
+
+
+
+// fin  multiplie
+   }
+ 
+  fclose(fichierjo);
+  fclose(fichierjoa);
+}
+
+// fin de la zone de travail de l'apprenant
