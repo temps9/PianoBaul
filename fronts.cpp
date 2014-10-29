76e0317f801acbf9b2a450c9c4e71a07776a27d9
André pierre <temps.jo@gmail.com>
2014-10-29 23:43:43 +0100

    Update fronts.cpp
    game

--- fronts.cpp
+++ fronts.cpp
@@ -1,320 +1,90 @@
 #include "fronts.h"
 #include "jo.h"
+
+
 jo clavier32[26] = {
    { // a touche1
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 85 , 84 , 169, 168, 85, 84, 169, 168, 85, 84, 169, 168, 169, 168}, // dureefront
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+448
    },
    { // b touche25
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 21 , 21 , 42, 42, 21, 21, 42, 42, 21, 21, 42, 42, 42, 42},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+112
    },
    { // c touche23
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 24 , 23 , 47, 47, 24, 23, 47, 47, 24, 23, 47, 47, 47, 47},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+126
    },
    { // d touche13
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 42 , 42 , 84, 84, 42, 42, 84, 84, 42, 42, 84, 84, 84, 84},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+224
    },
-   { // e
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 75 , 75 , 150, 150, 75, 75, 150, 150, 75, 75, 150, 150, 150, 150},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+   { // e touche3
+400
    },
    { // f touche14
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 40 , 40 , 80, 79, 40, 40, 80, 79, 40, 40, 80, 79, 80, 79},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+212
    },
    { // g touche15
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 38 , 37 , 75, 75, 38, 37, 75, 75, 38, 37, 75, 75, 75, 75},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+200
    },
    { // h touche16
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 35 , 36 , 71, 71, 35, 36, 71, 71, 35, 36, 71, 71, 71, 71},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+189
    },
    { // i touche8
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 56 , 56 , 113, 112, 56, 56, 113, 112, 56, 56, 113, 112, 113, 112},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+300
    },
    { // j touche17
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 34 , 33 , 67, 67, 34, 33, 67, 67, 34, 33, 67, 67, 67, 67},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+178
    },
    { // k touche18
- { 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
- { 32 , 31 , 63, 63, 32, 31, 63, 63, 32, 31, 63, 63, 63, 63},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+168
    },
    { // l touche19
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 30 , 30 , 60, 59, 30, 30, 60, 59, 30, 30, 60, 59, 60, 59},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+159
    },
    { // m touche20
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 28 , 28 , 56, 56, 28, 28, 56, 56, 28, 28, 56, 56, 56, 56},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+150
    },
    { // n touche26
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 20 , 20 , 40, 40, 20, 20, 40, 40, 20, 20, 40, 40, 40, 40},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+106
    },
    { // o touche9
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 53 , 53 , 106, 106, 53, 53, 106, 106, 53, 53, 106, 106, 106, 106},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+283
    },
    { // p touche10
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 50 , 50 , 100, 100, 50, 50, 100, 100, 50, 50, 100, 100, 100, 100},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+267
    },
    { // q touche11
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 47 , 47 , 95, 94, 47, 47, 95, 94, 47, 47, 95, 94, 95, 94},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+252
    },
 
    { // r touche4
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 71 , 71 , 142, 142, 71, 71, 142, 142, 71, 71, 142, 142, 142, 142},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+378
    },
    { // s touche12
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 44 , 45 , 89, 89, 44, 45, 89, 89, 44, 45, 89, 89, 89, 89},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+238
    },
    { // t touche5
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 67 , 67 , 134, 134, 67, 67, 134, 134, 67, 67, 134, 134, 134, 134},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+356
    },
    { // u touche7
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 60 , 59 , 119, 119, 60, 59, 119, 119, 60, 59, 119, 119, 119, 119},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+318
    },
    { // v touche24
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 22 , 22 , 45, 44, 22, 22, 45, 44, 22, 22, 45, 44, 45, 44},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+119
    },
    { // w touche21
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 27 , 26 , 53, 53, 27, 26, 53, 53, 27, 26, 53, 53, 53, 53},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+141
    },
    { // x touche22
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 25 , 25 , 50, 50, 25, 25, 50, 50, 25, 25, 50, 50, 50, 50},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+133
    },
 
    { // y touche6
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 63 , 63 , 126, 126, 63, 63, 126, 126, 63, 63, 126, 126, 126, 126},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
+336
    },
-   { // z
-{ 133 , 130 , 133, 130, 133, 130, 133, 130, 133, 130, 133, 130, 133, 130}, // amplitude
-{ 80 , 79 , 159, 159, 80, 79, 159, 159, 80, 79, 159, 159, 159, 159},
-{ 12 , 11 , 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1}, // changeamplitude
-{ 0 , 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // changedureefront
-1 , // duree
-7 , // laforceh
-21 , // laforceb
-7 , // tremolo
-21 , // forceplus
-2  // combiendezonememoire
-   }
-};
+   { // z touche2
+424
+}
+    };
 
 jo *mesjo = clavier32;
+
+
