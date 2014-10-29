2ca88611d4b065ec953533da75e8b5f10bf4c179
André pierre <temps.jo@gmail.com>
2014-10-29 23:44:49 +0100

    Update jo.h

--- jo.h
+++ jo.h
@@ -3,16 +3,7 @@
 
 struct jo
 {
-  int amplitude[14];
-  int dureefront[14];
-  int changeamplitude[14];
-  int changedureefront[14];
-  int duree;
-  int laforceh;
-  int laforceb;
-  int tremolo;
-  int forceplus;
-  int combiendezonememoire;
+  int lagame;
 };
 
 #endif // _jo_h_
