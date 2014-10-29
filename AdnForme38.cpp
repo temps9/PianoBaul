7adba70578efb78e2a7218836f7b41d4559e0375
André pierre <temps.jo@gmail.com>
2014-10-29 23:38:54 +0100

    Create AdnForme38.cpp

---
+++ AdnForme38.cpp
@@ -0,0 +1,75 @@
+#include "AdnForme38.h"
+
+// Génère les fichiers c.jo et c.joa
+void genereFichierJo38(char c)
+{
+
+    int mesamplifront[2]; //son du n, la référence
+    int mesdurerfront[2]; //son du n, la référence
+    int mesvarampli[2]; //son du n, la référence
+    int mesvardurer[2]; //son du n, la référence
+    int madurer; //son du n, la référence
+    int maforceh;
+    int maforceb;
+    int montremolo;
+    int maforceplus;
+    int macombiendezonememoire;
+
+
+   FILE* fichierdesfronts = NULL;
+
+ 
+    fichierdesfronts = fopen("fronts.txt", "r");
+ 
+    if (fichierdesfronts != NULL)
+    {
+        fscanf(fichierdesfronts, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d", &mesamplifront[0], &mesamplifront[1], &mesdurerfront[0], &mesdurerfront[1], &mesvarampli[0], &mesvarampli[1], &mesvardurer[0], &mesvardurer[1], &madurer, &maforceh, &maforceb, &montremolo, &maforceplus, &macombiendezonememoire);       
+ 
+        fclose(fichierdesfronts);
+
+    }
+
+/**********************************************/
+ jo joc = mesjo[c-'a'];
+
+  // Nom du fichier jo
+  char nomfichierjo[32];
+  snprintf(nomfichierjo, 32, "%c.jo", c);
+  // Nom du fichier joa
+  char nomfichierjoa[32];
+  snprintf(nomfichierjoa, 32, "%c.joa", c);
+
+  // Fichier jo
+  FILE* fichierjo;
+  if (!(fichierjo = fopen(nomfichierjo, "wb")))
+   {
+    exit(-1);
+   }
+  // Fichier joa
+  FILE* fichierjoa;
+  if (!(fichierjoa = fopen(nomfichierjoa, "wb")))
+   {
+    exit(-1);
+   }
+
+// ici commence la zone de travail pour l'apprenant
+
+  for(int duree = maforceh; duree>0; duree--)
+   {
+
+    for(int laforcebbase = maforceb; laforcebbase > 0; laforcebbase--)
+     {
+      for(int i=0; i<2; i++)
+       {
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[i], (char)mesdurerfront[i]);
+        fprintf(fichierjoa, "%d %d\n", (unsigned char)mesamplifront[i], (unsigned char)mesdurerfront[i]);
+
+       }
+     }
+     }
+ 
+  fclose(fichierjo);
+  fclose(fichierjoa);
+}
+
+// fin de la zone de travail de l'apprenant
