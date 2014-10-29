006a58ead65868419a1b88c7fce89560477bc8a5
André pierre <temps.jo@gmail.com>
2014-10-29 23:45:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,10 +10,64 @@
 /*Cette oeuvre est protégée et n'est divulguée et réservé que pour utilisation sous linux dans les conditions Creative common ci-dessous*/
 /* Cette oeuvre est mise à disposition selon les termes de la Licence Creative Commons Paternité - Pas d'Utilisation Commerciale - Partage des Conditions Initiales à l'Identique 2.0 France. */
 /*Aucune garantie n'est donner sur les effets de cet executable*/
+/*depend de sudo apt-get install qtmultimedia5-dev
+sudo apt-get install libqt5multimedia5-plugins*/
+
+#ifdef QT_STATIC
+#include <QtCore/QtPlugin>
+Q_IMPORT_PLUGIN(DSServicePlugin)
+#endif
+
+
 int main(int argc, char *argv[])
 {
+
+
+
+   FILE* fichier = NULL;
+   fichier = fopen ("fronts.txt", "w+");
+    if (fichier != NULL)
+    {
+          fprintf(fichier, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", 133, 130, 50, 50, 12, 12, 0, 0, 1, 7, 50, 5, 10, 2);
+        fclose(fichier);
+    }
+
+
+  char nomfichierwav[32];
+  char nomfichierwavacte[40];
+  char lettresclaviers[] = {97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123};
+  char c;
+        for(int i=0; i<26; i++)
+         {
+    c = lettresclaviers[i];
+    for(int n=1; n<6; n++)
+     {
+  snprintf(nomfichierwav, 32, "%c%d.wav", c, n);
+  FILE * fichierwav;
+  if (!(fichierwav=fopen(nomfichierwav, "r")))
+  // sinon, on le génère
+   {
+if (n==1)
+  {
+    genereFichierJo(c);
+    conversionJoWav(c,n);
+  }
+else
+  {    
+  snprintf(nomfichierwavacte, 40, "sox %c1.wav %c%d.wav", c, c, n);
+system(nomfichierwavacte);
+  }
+   }
+  else
+   fclose(fichierwav);
+}
+}
+
     QApplication app(argc, argv);
     clavier fenetre;
     fenetre.show();
     return app.exec();
 }
+
+
+
