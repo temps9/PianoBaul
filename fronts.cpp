e61805f3e8061b0942aae564cef0bde7366e4bfd
André pierre <temps.jo@gmail.com>
2014-11-04 21:44:42 +0100

    Update fronts.cpp
    correctif durée totale

--- fronts.cpp
+++ fronts.cpp
@@ -4,84 +4,110 @@
 
 jo clavier32[26] = {
    { // a touche1
-448
+448, // lagame
+22  // invgame
    },
    { // b touche25
-112
+112, // lagame
+89  // invgame
    },
    { // c touche23
-126
+126, // lagame
+79  // invgame
    },
    { // d touche13
-224
+224, // lagame
+45  // invgame
    },
    { // e touche3
-400
+400, // lagame
+25  // invgame
    },
    { // f touche14
-212
+212, // lagame
+47  // invgame
    },
    { // g touche15
-200
+200, // lagame
+50  // invgame
    },
    { // h touche16
-189
+189, // lagame
+53  // invgame
    },
    { // i touche8
-300
+300, // lagame
+33  // invgame
    },
    { // j touche17
-178
+178, // lagame
+56  // invgame
    },
    { // k touche18
-168
+168, // lagame
+60  // invgame
    },
    { // l touche19
-159
+159, // lagame
+63  // invgame
    },
    { // m touche20
-150
+150, // lagame
+67  // invgame
    },
    { // n touche26
-106
+106, // lagame
+94  // invgame
    },
    { // o touche9
-283
+283, // lagame
+35  // invgame
    },
    { // p touche10
-267
+267, // lagame
+37  // invgame
    },
    { // q touche11
-252
+252, // lagame
+40  // invgame
    },
 
    { // r touche4
-378
+378, // lagame
+26  // invgame
    },
    { // s touche12
-238
+238, // lagame
+42  // invgame
    },
    { // t touche5
-356
+356, // lagame
+28  // invgame
    },
    { // u touche7
-318
+318, // lagame
+31  // invgame
    },
    { // v touche24
-119
+119, // lagame
+84  // invgame
    },
    { // w touche21
-141
+141, // lagame
+71  // invgame
    },
    { // x touche22
-133
+133, // lagame
+75  // invgame
    },
 
    { // y touche6
-336
+336, // lagame
+30  // invgame
    },
    { // z touche2
-424
+424, // lagame
+24  // invgame
 }
     };
 
