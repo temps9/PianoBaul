ecea6d601d511c20a01b6309de56695a8fb89f51
André pierre <temps.jo@gmail.com>
2014-11-07 00:20:49 +0100

    Update AdnForme14.cpp

--- AdnForme14.cpp
+++ AdnForme14.cpp
@@ -54,19 +54,250 @@ void genereFichierJo14(char c)
 
 // ici commence la zone de travail pour l'apprenant
 
-  for(int duree = maforceh; duree>0; duree--)
+
+
+mesdurerfront[0] = ((joc.lagame * mesdurerfront[0]) * 0.01);
+mesdurerfront[1] = ((joc.lagame * mesdurerfront[1]) * 0.01);
+maforceh = ((joc.invgame * maforceh) * 0.1);
+maforceb = ((joc.invgame * maforceb) * 0.1);
+      for(int i=0; i<2; i++)
+       {
+         mesvarampli[i] = ((joc.lagame * mesvarampli[i]) * 0.01);
+	}
+
+int fronts1[2];
+int fronts2[2];
+
+if(mesamplifront[0]>128)
+{
+ fronts1[0] = mesamplifront[0] -128;
+ fronts1[1] = 128 - fronts1[0];
+}
+if(mesamplifront[0]<128)
+{
+ fronts1[0] = 128 - mesamplifront[0];
+ fronts1[1] = 128 + fronts1[0];
+}
+if(mesamplifront[0]==128)
+{
+ fronts1[0] = 129;
+ fronts1[1] = 127;
+}
+if(mesamplifront[1]>128)
+{
+ fronts2[0] = mesamplifront[1] -128;
+ fronts2[1] = 128 - fronts2[0];
+}
+if(mesamplifront[1]<128)
+{
+ fronts2[0] = 128 - mesamplifront[1];
+ fronts2[1] = 128 + fronts2[0];
+}
+if(mesamplifront[1]==128)
+{
+ fronts2[0] = 129;
+ fronts2[1] = 127;
+}
+
+// multiplie
+  for(int duree = madurer; duree>0; duree--)
    {
 
-    for(int laforcebbase = maforceb; laforcebbase > 0; laforcebbase--)
+//monte
+
+    for(int laforcebbase = maforceh; laforcebbase > 0; laforcebbase--)
      {
       for(int i=0; i<2; i++)
        {
-        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[i], (char)mesdurerfront[i]);
-        fprintf(fichierjoa, "%d %d\n", (unsigned char)mesamplifront[i], (unsigned char)mesdurerfront[i]);
+        if(i%2)
+         fronts1[i] -= mesvarampli[0];
+        else
+         fronts1[i] += mesvarampli[0];
+	
+	 // respect des limites
+	if(fronts1[i]>239)
+	fronts1[i] = 239;
+
+	if(fronts1[i]<16)
+	fronts1[i] = 16;	
+
+        fprintf(fichierjo,  "%c%c",    (char)fronts1[i], (char)mesdurerfront[i]);
+        fprintf(fichierjoa, "%d %d\n", (unsigned char)fronts1[i], (unsigned char)mesdurerfront[i]);
+
+       }
+      for(int i=0; i<2; i++)
+       {
+        if(i%2)
+         fronts2[i] -= mesvarampli[1];
+        else
+         fronts2[i] += mesvarampli[1];
+	
+	 // respect des limites
+	if(fronts2[i]>239)
+	fronts2[i] = 239;
+
+	if(fronts2[i]<16)
+	fronts2[i] = 16;	
+
+        fprintf(fichierjo,  "%c%c",    (char)fronts2[i], (char)mesdurerfront[i]);
+        fprintf(fichierjoa, "%d %d\n", (unsigned char)fronts2[i], (unsigned char)mesdurerfront[i]);
 
        }
      }
+//plateau
+int basculeDe1 = montremolo * 2;
+int basculeDe2 = basculeDe1 - 1;
+int basculeDe3 = basculeDe1 * 2 - 1;
+int basculeDe5 = 0;
+
+    for(int laforcebbase = maforceb; laforcebbase > 0; laforcebbase--)
+     {
+
+
+if(basculeDe5<basculeDe1)
+{
+mesdurerfront[0] += mesvardurer[0];
+fronts1[0] -= mesvarampli[0];
+fronts2[0] -= mesvarampli[0];
+mesdurerfront[1] += mesvardurer[1];
+fronts1[1] -= mesvarampli[1];
+fronts2[1] -= mesvarampli[1];
+}
+
+if(basculeDe5>basculeDe2)
+{
+mesdurerfront[0] -= mesvardurer[0];
+fronts1[0] += mesvarampli[0];
+fronts2[0] += mesvarampli[0];
+mesdurerfront[1] -= mesvardurer[1];
+fronts1[1] += mesvarampli[1];
+fronts2[1] += mesvarampli[1];
+}
+basculeDe5++;
+if(basculeDe5>basculeDe3)
+basculeDe5 = 0;
+
+      for(int i=0; i<2; i++)
+       {
+
+	if(fronts1[i]>239)
+	fronts1[i] = 239;
+
+	if(fronts1[i]<16)
+	fronts1[i] = 16;
+
+	if(fronts2[i]>239)
+	fronts2[i] = 239;
+
+	if(fronts2[i]<16)
+	fronts2[i] = 16;
+
+	if(mesdurerfront[i]>239)
+	mesdurerfront[i] = 239;
+
+	if(mesdurerfront[i]<6)
+	mesdurerfront[i] = 6;
+
+        fprintf(fichierjo,  "%c%c",    (char)fronts1[i], (char)mesdurerfront[i]);
+        fprintf(fichierjoa, "%d %d\n", (unsigned char)fronts1[i], (unsigned char)mesdurerfront[i]);
+        fprintf(fichierjo,  "%c%c",    (char)fronts2[i], (char)mesdurerfront[i]);
+        fprintf(fichierjoa, "%d %d\n", (unsigned char)fronts2[i], (unsigned char)mesdurerfront[i]);
+
+       }
+int fronts1a[2];  
+int fronts2a[2];
+      for(int i=0; i<2; i++)
+       {
+fronts1a[i] = fronts1[i];
+fronts2a[i] = fronts2[i];
+}
+
+fronts1a[0] = ((joc.rapportvingtsix * fronts1a[0]) * 0.001);
+fronts2a[0] = ((joc.rapportvingtsix * fronts2a[0]) * 0.001); 
+
+      for(int aaee=maforceplus; aaee>0; aaee--)
+       {
+      for(int i=0; i<2; i++)
+       {
+
+	if(fronts1a[i]>239)
+	fronts1a[i] = 239;
+
+	if(fronts1a[i]<16)
+	fronts1a[i] = 16;
+
+	if(fronts2a[i]>239)
+	fronts2a[i] = 239;
+
+	if(fronts2a[i]<16)
+	fronts2a[i] = 16;
+
+	if(mesdurerfront[i]>239)
+	mesdurerfront[i] = 239;
+
+	if(mesdurerfront[i]<6)
+	mesdurerfront[i] = 6;
+
+        fprintf(fichierjo,  "%c%c",    (char)fronts1a[i], (char)mesdurerfront[i]);
+        fprintf(fichierjoa, "%d %d\n", (unsigned char)fronts1a[i], (unsigned char)mesdurerfront[i]);
+        fprintf(fichierjo,  "%c%c",    (char)fronts2a[i], (char)mesdurerfront[i]);
+        fprintf(fichierjoa, "%d %d\n", (unsigned char)fronts2a[i], (unsigned char)mesdurerfront[i]);
+
+       }
+       }
+
+
+
      }
+
+
+
+//descend
+
+    for(int laforcebbase = maforceh; laforcebbase > 0; laforcebbase--)
+     {
+      for(int i=0; i<2; i++)
+       {
+        if(i%2)
+         fronts2[i] += mesvarampli[1];
+        else
+         fronts2[i] -= mesvarampli[1];
+	
+	 // respect des limites
+	if(fronts2[i]>239)
+	fronts2[i] = 239;
+
+	if(fronts2[i]<16)
+	fronts2[i] = 16;	
+
+        fprintf(fichierjo,  "%c%c",    (char)fronts2[i], (char)mesdurerfront[i]);
+        fprintf(fichierjoa, "%d %d\n", (unsigned char)fronts2[i], (unsigned char)mesdurerfront[i]);
+
+       }
+      for(int i=0; i<2; i++)
+       {
+        if(i%2)
+         fronts1[i] += mesvarampli[0];
+        else
+         fronts1[i] -= mesvarampli[0];
+	
+	 // respect des limites
+	if(fronts1[i]>239)
+	fronts1[i] = 239;
+
+	if(fronts1[i]<16)
+	fronts1[i] = 16;	
+
+        fprintf(fichierjo,  "%c%c",    (char)fronts1[i], (char)mesdurerfront[i]);
+        fprintf(fichierjoa, "%d %d\n", (unsigned char)fronts1[i], (unsigned char)mesdurerfront[i]);
+
+       }
+     }
+
+
+
+// fin  multiplie
+   }
  
   fclose(fichierjo);
   fclose(fichierjoa);
